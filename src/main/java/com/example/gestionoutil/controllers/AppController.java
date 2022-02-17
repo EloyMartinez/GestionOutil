40dcce2bfa08d1aafcfc071fad2c148b8fc40ad9
ImArxus <alex.lebalanger@icloud.com>
2022-02-17 13:11:55 -0500

    Inscription et connexion OK

--- src/main/java/com/example/gestionoutil/controllers/AppController.java
+++ src/main/java/com/example/gestionoutil/controllers/AppController.java
@@ -2,8 +2,6 @@ package com.example.gestionoutil.controllers;
 
 import com.example.gestionoutil.dao.ClientDAO;
 import com.example.gestionoutil.entity.MyClientEntity;
-import com.example.gestionoutil.repositories.ClientRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,12 +10,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AppController {
-    @Autowired
-    private ClientRepository clientRepository;
 
     @GetMapping("")
     public String viewHomePage(){
-        return "index";
+        return "login";
     }
 
     @GetMapping("/login")
@@ -29,12 +25,6 @@ public class AppController {
     public String register(Model model) {
         model.addAttribute("client", new MyClientEntity());
         return "register";
-    }
-
-    @GetMapping("/register2")
-    public String register2(Model model) {
-        model.addAttribute("client", new MyClientEntity());
-        return "register2";
     }
 
     @GetMapping("/users")
@@ -52,13 +42,4 @@ public class AppController {
         return "register_success";
     }
 
-    @PostMapping("/process_register2")
-    public String processRegister2(MyClientEntity client) {
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        client.setMdpcli(passwordEncoder.encode(client.getMdpcli()));
-        ClientDAO clientDAO= new ClientDAO();
-        clientDAO.save(client);
-
-        return "register_success2";
-    }
 }
