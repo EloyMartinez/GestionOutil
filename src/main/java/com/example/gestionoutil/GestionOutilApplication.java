cfdc25eccc002acce4f226e21f79c1dcfb512bfd
alexprime100 <alexprime100@gmail.com>
2022-02-19 09:13:13 -0500

    a

--- src/main/java/com/example/gestionoutil/GestionOutilApplication.java
+++ src/main/java/com/example/gestionoutil/GestionOutilApplication.java
@@ -8,6 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import sun.security.acl.AclEntryImpl;
 
+import java.sql.Date;
+
 @SpringBootApplication
 public class GestionOutilApplication {
     public static void main(String[] args) {
@@ -15,11 +17,26 @@ public class GestionOutilApplication {
 
         /*ClientDAO clientDAO = new ClientDAO();
         MyClientEntity client = new MyClientEntity();
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         client.setNomcli("nom");
         client.setPrenomcli("prenom");
         client.setAdmin(false);
         client.setCourrielcli("courriel");
+        client.setMdpcli(passwordEncoder.encode("azert"));
+        client.setTelephonecli("56465");
+        client.setDatenaissancecli(new Date(1,1,1));
 
-        clientDAO.save(client);*/
+        MyClientEntity client1 = new MyClientEntity();
+        client1.setNomcli("admin");
+        client1.setPrenomcli("admin");
+        client1.setAdmin(true);
+        client1.setCourrielcli("admin@mail.com");
+        client1.setMdpcli(passwordEncoder.encode("azert"));
+        client1.setTelephonecli("56465");
+        client1.setDatenaissancecli(new Date(1,1,1));
+
+
+        clientDAO.save(client);
+        clientDAO.save(client1);*/
     }
 }
